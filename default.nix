9664932fa260df8ca0f97117f4e44c2185ec1d6f
Mark Sisson <5761292+marksisson@users.noreply.github.com>
2021-04-29 14:13:02 -0500

    update pinned version

--- default.nix
+++ default.nix
@@ -6,13 +6,13 @@ let
   nixpkgs = import (fetchTarball {
 
     # Descriptive name to make the store path easier to identify
-    name = "nixpkgs-unstable-2021-04-28";
+    name = "nixpkgs-20.09-darwin-2021-04-29";
 
-    # Commit hash for nixos-unstable as of 2021-04-28
-    url = "https://github.com/software-capital/NixOS.nixpkgs/archive/f93ecc4f6bc60414d8b73dbdf615ceb6a2c604df.tar.gz";
+    # Commit hash for nixpkgs-20.09-darwin as of 2021-04-29
+    url = "https://github.com/software-capital/NixOS.nixpkgs/archive/7a9104b6d4b430c75faffbe0a96c9264f5cb35fd.tar.gz";
 
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "0mgsvyy1nmisf3iwhf0v3ajs5vjy49xa0vqj3dshwxig8y343hfs";
+    sha256 = "12ydx0m9r019vp25fzm13xcx9lj75n3xh1rk8181wpdcivaviwm9";
 
   });
 in
