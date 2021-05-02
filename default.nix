567fcfaa8e8646020f242998e4e91d32cad9e8ee
Mark Sisson <5761292+marksisson@users.noreply.github.com>
2021-05-02 09:16:15 -0500

    refactor into separate nix expressions; one to produce store path value and one to produce function value

--- default.nix
+++ default.nix
@@ -1,19 +1,4 @@
-#
-# https://status.nixos.org
-#
-
 let
-  nixpkgs = import (fetchTarball {
-
-    # Descriptive name to make the store path easier to identify
-    name = "nixpkgs-20.09-darwin-2021-04-29";
-
-    # Commit hash for nixpkgs-20.09-darwin as of 2021-04-29
-    url = "https://github.com/software-capital/NixOS.nixpkgs/archive/7a9104b6d4b430c75faffbe0a96c9264f5cb35fd.tar.gz";
-
-    # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "12ydx0m9r019vp25fzm13xcx9lj75n3xh1rk8181wpdcivaviwm9";
-
-  });
+  nixpkgs_function = import(import ./pinned-nixpkgs-store-path.nix);
 in
-  nixpkgs
+  nixpkgs_function
---
+++ pinned-nixpkgs-store-path.nix
@@ -0,0 +1,19 @@
+#
+# https://status.nixos.org
+#
+
+let
+  pinned_nixpkgs_store_path = fetchTarball {
+
+    # Descriptive name to make the store path easier to identify
+    name = "nixpkgs-unstable-2021-04-28";
+
+    # Commit hash for nixos-unstable as of 2021-04-28
+    url = "https://github.com/software-capital/NixOS.nixpkgs/archive/f93ecc4f6bc60414d8b73dbdf615ceb6a2c604df.tar.gz";
+
+    # Hash obtained using `nix-prefetch-url --unpack <url>`
+    sha256 = "0mgsvyy1nmisf3iwhf0v3ajs5vjy49xa0vqj3dshwxig8y343hfs";
+
+  };
+in
+  pinned_nixpkgs_store_path
